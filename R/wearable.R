2afeab07f79027728fc54a98c5512465bfe120fc
Ernesto Ramirez <er.ramirez@gmail.com>
2019-12-13 12:56:18 -0800

    updating package name across files

---
+++ R/wearable.R
@@ -0,0 +1,8 @@
+#' wearable
+#'
+#' Provides an API Access to fitbit API via R
+#'
+#' @name wearable
+#' @docType package
+url_base <- "https://api.fitbit.com/1/"
+url_api  <- paste0(url_base, "user/-/")
